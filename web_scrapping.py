0f0b991f7cb6296f77d619903ee04e8aac9ffce2
Ahamudul Hasan <118659753+Priantox@users.noreply.github.com>
2025-07-31 00:28:53 +0600

    changes

--- web_scrapping.py
+++ web_scrapping.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-url = 'https://www.uiu.ac.bd/'
+url = 'https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36'
@@ -12,5 +12,12 @@ webpage = requests.get(url,headers=headers).text
 
 soup = BeautifulSoup(webpage,'lxml')
 
+# print(soup.prettify())
+
 for name in soup.find_all('h1'):
     print(name.text.strip())
+
+
+for i in soup.find_all('p'):
+    print(i.text.strip())
+
